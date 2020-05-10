9951212d9c756e76b9168dc2e2f4aa55c05a9f94
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-05-10 18:38:12 +0200

    restore the audio

--- test.m
+++ test.m
@@ -4,34 +4,52 @@ clear all;
 number_of_samples1 = length(x1);
 duration_in_seconds1 = floor(number_of_samples1 / fs1);
 % sound(x1, fs1);
+% pause(duration_in_seconds1);
 
 [x2, fs2] = audioread("audio2.wav");
 number_of_samples2 = length(x2);
 duration_in_seconds2 = floor(number_of_samples2 / fs2);
 % sound(x2, fs2);
+% pause(duration_in_seconds2);
+
+[~,peaks1] = findpeaks(x1);
+N1 = mean(diff(peaks1));
+[~,peaks2] = findpeaks(x2);
+N2 = mean(diff(peaks2));
+omega = 2*pi/N1;  %omega = 2pi/period
+A = 0.1;
 
 X = x2;
-[~,peaklocs] = findpeaks(x1);
-N = mean(diff(peaklocs));
-omega = (2*pi)/N;
 for n = 1 : number_of_samples1
-    disp(X(n));
-    X(n) = X(n)+0.1*x1(n)*cos(omega*n);
-    disp(X(n));
+    X(n) = x2(n)+A*x1(n)*cos(omega*n);
 end
-audiowrite("result.wav",X,fs2);
-plot(log10(abs(fft(X))));
 sound(X, fs2);
+pause(duration_in_seconds2);
+
+audiowrite("result.wav",X,fs2);
+% figure('Name', 'the magnitude spectrum for the result'); plot(log10(abs(fft(X))));
+figure('Name', 'the magnitude spectrum for audio1'); plot((abs(fft(x1))));
+% figure('Name', 'the magnitude spectrum for audio2'); plot(log10(abs(fft(x2))));
+
 
 Y = linspace(0, duration_in_seconds1, number_of_samples1);
 for n = 1 : number_of_samples1
     Y(n) = X(n)*cos(omega*n);
 end
-% sound(Y);
-% title("audio1");
-% X1 = linspace(0, duration_in_seconds1, number_of_samples1);
-% plot(X1, x1);
-% 
-% title("audio2");
-% X2 = linspace(0, duration_in_seconds2, number_of_samples2);
-% plot(X2, x2);
+% frequency domain; fourier transform
+Yfft = fft(Y);
+% multiply the range of Y[k] by zeros.
+range = 8;
+for k = floor(length(Yfft)/range) : (range)*floor(length(Yfft)/range)
+    Yfft(k) = 0;
+end
+% delete the effect of the attenuation factor by dividing by it.
+for n = 1 : length(Yfft)
+    Yfft(n) = Yfft(n) / A;
+end
+% inverse fourier transform
+Yifft = abs(ifft(Yfft));
+audiowrite("restored.wav",Yifft,fs1);
+sound(Yifft, fs1);
+pause(duration_in_seconds1);
+figure('Name', 'the magnitude spectrum for restored audio'); plot((abs(fft(Y))));
